d3273e3a2330d60b92b6afbbd3dc35d004120f33
Caesar Gheorghe <caesar.gheorghe@bolt.eu>
2023-05-22 12:12:02 +0300

    Second Function

--- src/index.js
+++ src/index.js
@@ -9,3 +9,15 @@ function map(items, f) {
 }
 var i = ["1", "2", "3"];
 console.log(map(i, function (x) { return x + "1"; }));
+function filter(items, predicate) {
+    var result = [];
+    for (var _i = 0, items_2 = items; _i < items_2.length; _i++) {
+        var item = items_2[_i];
+        if (predicate(item)) {
+            result.push(item);
+        }
+    }
+    return result;
+}
+var j = ["1", "2", "3"];
+console.log(filter(j, function (x) { return x !== "2"; }));
--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,16 @@ function map(items: string[], f: (string) => string): string[] {
     return result
 }
 let i=["1","2","3"];
-
 console.log (map(i, (x) => x + "1"))
+function filter(items:string[], predicate: (string) => boolean): string[]{
+    let result: string []=[];
+    for (let item of items){
+        if (predicate(item)){
+            result.push (item)
+        }
+        
+    }
+    return result
+}
+let j = ["1","2","3"];
+console.log (filter(j, (x) => x!=="2"))
